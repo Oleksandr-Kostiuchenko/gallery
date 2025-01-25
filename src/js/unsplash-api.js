bf5bba9c2d814e150d6dca4c7f930346a37da15e
Oleksandr Kostiuchenko <aleksandrkostucenko11@gmail.com>
2025-01-25 10:14:18 +0100

    add-fetch-function

--- src/js/unsplash-api.js
+++ src/js/unsplash-api.js
@@ -1,1 +1,21 @@
-console.log('Hello');
+//* Import library 
+import axios from "axios";
+import { apiKey } from "./api-key";
+
+//* Get function 
+const getPhotos = async (query, page) => {
+    const baseURL = 'https://api.unsplash.com';
+    const endpoint = '/search/photos';
+
+    const response = await axios.get(`${baseURL}${endpoint}`, {
+        params: {
+            query,
+            page,
+            per_page: 12,
+            orientation: 'portrait',
+            client_id: apiKey
+        }
+    });
+
+    return response.data;
+}
